cb6ed53ea55b7ceda395ff7e53d569e79598800d
davidenco88 <122131492+davidenco88@users.noreply.github.com>
2024-01-22 16:53:44 +0100

    feat: :sparkles: Add Step 3 - Integrating Socket.IO

--- SOCKET.IO/index.js
+++ SOCKET.IO/index.js
@@ -2,21 +2,28 @@ import express from "express";
 import { createServer } from "node:http";
 import { fileURLToPath } from "node:url";
 import { dirname, join } from "node:path";
+import { Server } from "socket.io";
+import { Socket } from "engine.io";
 
 const app = express();
 const server = createServer(app);
-
+const io = new Server(server);
 // utiliza la convensión de Node.js para nombrar variables especiales
-// en este caso una varibales del entorno de ejecución.
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 app.get("/", (req, res) => {
   res.sendFile(join(__dirname, "index.html"));
 });
 
+io.on("connection", (socket) => {
+  console.log("Indentify user connection");
+  socket.on("disconnect", () => {
+    console.log("Indentify user disconnection");
+  });
+});
+
 const PORT = process.env.PORT ?? 3000;
 
 server.listen(PORT, () => {
   console.log(`Listening on http://localhost:${PORT}`);
-  console.log(__dirname);
 });
