6cc6c10b5712f3a8a8277098284e08b00af12ecb
davidenco88 <122131492+davidenco88@users.noreply.github.com>
2024-01-22 16:06:30 +0100

    feat: :sparkles: Add Step 2 - Serving HTML

--- SOCKET.IO/index.js
+++ SOCKET.IO/index.js
@@ -1,15 +1,22 @@
 import express from "express";
 import { createServer } from "node:http";
+import { fileURLToPath } from "node:url";
+import { dirname, join } from "node:path";
 
 const app = express();
 const server = createServer(app);
 
+// utiliza la convensión de Node.js para nombrar variables especiales
+// en este caso una varibales del entorno de ejecución.
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 app.get("/", (req, res) => {
-  res.send("<h1>Start Socket IO</h1>");
+  res.sendFile(join(__dirname, "index.html"));
 });
 
 const PORT = process.env.PORT ?? 3000;
 
 server.listen(PORT, () => {
   console.log(`Listening on http://localhost:${PORT}`);
+  console.log(__dirname);
 });
